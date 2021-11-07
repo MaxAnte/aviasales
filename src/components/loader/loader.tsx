0ff0a7c254b951a6fd1b2f232b95afd6daef4203
MaxAnte <qzzik97@gmail.com>
2021-11-07 13:40:02 +0200

    changed loader

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -4,87 +4,86 @@ import styled from "styled-components";
 const StyledLoader = styled.div`
   display: inline-block;
   position: relative;
-  margin: 0 auto;
   width: 80px;
   height: 80px;
 
   div {
-    animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-    transform-origin: 40px 40px;
-    &:after {
-      content: " ";
-      display: block;
-      position: absolute;
-      width: 7px;
-      height: 7px;
-      border-radius: 50%;
-      background: #2196f3;
-      margin: -4px 0 0 -4px;
-    }
+    position: absolute;
+    width: 6px;
+    height: 6px;
+    background: #2196f3;
+    border-radius: 50%;
+    animation: lds-default 1.2s linear infinite;
     &:nth-child(1) {
-      animation-delay: -0.036s;
-    }
-    &:nth-child(1):after {
-      top: 63px;
-      left: 63px;
+      animation-delay: 0s;
+      top: 37px;
+      left: 66px;
     }
     &:nth-child(2) {
-      animation-delay: -0.072s;
-    }
-    &:nth-child(2):after {
-      top: 68px;
-      left: 56px;
+      animation-delay: -0.1s;
+      top: 22px;
+      left: 62px;
     }
     &:nth-child(3) {
-      animation-delay: -0.108s;
-      &:after {
-        top: 71px;
-        left: 48px;
-      }
+      animation-delay: -0.2s;
+      top: 11px;
+      left: 52px;
     }
     &:nth-child(4) {
-      animation-delay: -0.144s;
-      &:after {
-        top: 72px;
-        left: 40px;
-      }
+      animation-delay: -0.3s;
+      top: 7px;
+      left: 37px;
     }
     &:nth-child(5) {
-      animation-delay: -0.18s;
-      &:after {
-        top: 71px;
-        left: 32px;
-      }
+      animation-delay: -0.4s;
+      top: 11px;
+      left: 22px;
     }
     &:nth-child(6) {
-      animation-delay: -0.216s;
-      &:after {
-        top: 68px;
-        left: 24px;
-      }
+      animation-delay: -0.5s;
+      top: 22px;
+      left: 11px;
     }
     &:nth-child(7) {
-      animation-delay: -0.252s;
-      &:after {
-        top: 63px;
-        left: 17px;
-      }
+      animation-delay: -0.6s;
+      top: 37px;
+      left: 7px;
     }
-
     &:nth-child(8) {
-      animation-delay: -0.288s;
-      &:after {
-        top: 56px;
-        left: 12px;
-      }
+      animation-delay: -0.7s;
+      top: 52px;
+      left: 11px;
+    }
+    &:nth-child(9) {
+      animation-delay: -0.8s;
+      top: 62px;
+      left: 22px;
+    }
+    &:nth-child(10) {
+      animation-delay: -0.9s;
+      top: 66px;
+      left: 37px;
+    }
+    &:nth-child(11) {
+      animation-delay: -1s;
+      top: 62px;
+      left: 52px;
+    }
+    &:nth-child(12) {
+      animation-delay: -1.1s;
+      top: 52px;
+      left: 62px;
     }
   }
-  @keyframes lds-roller {
-    0% {
-      transform: rotate(0deg);
-    }
+  @keyframes lds-default {
+    0%,
+    20%,
+    80%,
     100% {
-      transform: rotate(360deg);
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.5);
     }
   }
 `;
@@ -92,6 +91,10 @@ const StyledLoader = styled.div`
 export default function Loader() {
   return (
     <StyledLoader>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
       <div></div>
       <div></div>
       <div></div>
